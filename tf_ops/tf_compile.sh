ed3ba68af7e063e323bd1d18e4162d0be92c328e
qbai <bbbaiqian@gmail.com>
2021-02-10 22:47:40 +0100

    correct directory

--- tf_ops/tf_compile.sh
+++ tf_ops/tf_compile.sh
@@ -1,5 +1,3 @@
 #/bin/bash
-TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
-TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
-/usr/local/cuda-9.0/bin/nvcc cuda_ulits.cu -o cuda_ulits.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
-g++ -std=c++11 tf_sampling.cpp tf_gather.cpp tf_interpolate.cpp cuda_ulits.cu.o -o tf_op_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include/external/nsync/public/ -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-9.0/include -lcudart -L /usr/local/cuda-9.0/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0 -I $TF_INC -L $TF_LIB -ltensorflow_framework
+/usr/local/cuda-11.0/bin/nvcc cuda_ulits.cu -o cuda_ulits.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
+g++ -std=c++11 tf_sampling.cpp tf_gather.cpp tf_interpolate.cpp cuda_ulits.cu.o -o tf_op_so.so -shared -fPIC -I /home/local/CYCLOMEDIA001/qbai/Documents/softwares/miniconda3/envs/GACNet/lib/python3.6/site-packages/tensorflow/include -I /usr/local/cuda-11.0/include -I /home/local/CYCLOMEDIA001/qbai/Documents/softwares/miniconda3/envs/GACNet/lib/python3.6/site-packages/tensorflow/include/external/nsync/public/ -lcudart -L /usr/local/cuda-11.0/lib64/ -L /home/local/CYCLOMEDIA001/qbai/Documents/softwares/miniconda3/envs/GACNet/lib/python3.6/site-packages/tensorflow -L /home/local/CYCLOMEDIA001/qbai/Documents/softwares/miniconda3/envs/GACNet/lib/python3.6/site-packages/tensorflow/lib -O2 -D_GLIBCXX_USE_CXX11_ABI=0 -ltensorflow_framework
